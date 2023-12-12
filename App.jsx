5cdc6a83ff8f811ebcd6ccbec1151904d05b3186
Arsyad Basofil Amri <arsyadbaso123@gmail.com>
2023-12-12 17:54:01 +0700

    [UPDATE] add icon for tab navigator

--- App.jsx
+++ App.jsx
@@ -11,9 +11,10 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
-
+import Icon from 'react-native-vector-icons/FontAwesome';
+import IconAntDesign from 'react-native-vector-icons/AntDesign';
 import HomeScreen from './src/screen/homeScreen';
 import DetailRecipes from './src/screen/detailRecipes';
 import RegisterScreen from './src/screen/registerScreen';
@@ -26,28 +27,26 @@ import ProfilScreen from './src/screen/profilScreen';
 import messaging from '@react-native-firebase/messaging';
 import firestore from '@react-native-firebase/firestore'; //import firestore
 
-import {
+import // StyleSheet, //berguna untuk styling di react native
 
-  // StyleSheet, //berguna untuk styling di react native
-
-} from 'react-native';
+'react-native';
 
 function App() {
-  const Stack = createNativeStackNavigator();//untuk navigation antar component
+  const Stack = createNativeStackNavigator(); //untuk navigation antar component
   const Tab = createBottomTabNavigator(); //navigasi bottom
 
-  const [token,setToken] = React.useState('');
+  const [token, setToken] = React.useState('');
 
   console.log(token);
 
   //funtion generate token device
-  const generateToken = async()=>{
+  const generateToken = async () => {
     const deviceToken = await messaging().getToken();
     setToken(deviceToken);
-};
+  };
 
   //function untuk meminta permisstion notif
-  const requestUserPermission = async()=> {
+  const requestUserPermission = async () => {
     const authStatus = await messaging().requestPermission();
     const enabled =
       authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
@@ -59,26 +58,74 @@ function App() {
     }
   };
 
-
   React.useEffect(() => {
     requestUserPermission(); //meminta AKSES permisstion notif saat app pertama kali di load
 
     //mengirim data token ke firebase, menggunakan doc agar tidak terjadi duplicate token
     firestore()
-            .collection('tokenList').doc(token).set({})
-            .then(() => {
-              console.log('token has saved');
-            });
+      .collection('tokenList')
+      .doc(token)
+      .set({})
+      .then(() => {
+        console.log('token has saved');
+      });
   }, []);
 
-
-  function Homes() {
+  function TabScreen() {//function untuk navigasi tabscreeen
     return (
       <Tab.Navigator>
-        <Tab.Screen name="Login" component={LoginScreen} options={{headerShown: false}}/>
-        <Tab.Screen name="Register" component={RegisterScreen} options={{headerShown: false}}/>
-        <Tab.Screen name="Home" component={HomeScreen} options={{headerShown: false}}/>
-        <Tab.Screen name="Profile" component={ProfilScreen} options={{headerShown: false}}/>
+        <Tab.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{
+            headerShown: false,
+            tabBarOptions: {
+              showIcon: true,
+            },
+            tabBarIcon: ({color, size}) => (
+              <IconAntDesign name="login" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            headerShown: false,
+            tabBarOptions: {
+              showIcon: true,
+            },
+            tabBarIcon: ({color, size}) => (
+              <Icon name="home" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Register"
+          component={RegisterScreen}
+          options={{
+            headerShown: false,
+            tabBarOptions: {
+              showIcon: true,
+            },
+            tabBarIcon: ({color, size}) => (
+              <Icon name="user-plus" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Profile"
+          component={ProfilScreen}
+          options={{
+            headerShown: false,
+            tabBarOptions: {
+              showIcon: true,
+            },
+            tabBarIcon: ({color, size}) => (
+              <Icon name="user" color={color} size={size} />
+            ),
+          }}
+        />
       </Tab.Navigator>
     );
   }
@@ -86,14 +133,34 @@ function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-          {/* name digunakan untuk link navigasi nantinya */}
-          {/* komponen adalah page yang akan di tampilkan */}
-          {/* component yang pertama kali dipanggil akan jadi homepage */}
-        <Stack.Screen name="Homes" component={Homes} options={{headerShown: false}}/>
-        <Stack.Screen name="Home" component={HomeScreen} options={{headerShown: false}}/>
-        <Stack.Screen name="Details" component={DetailRecipes} options={{headerShown: false}}/>
-        <Stack.Screen name="Popular" component={PopulraScreen} options={{headerShown: false}}/>
-        <Stack.Screen name="List" component={ListFood} options={{headerShown: false}}/>
+        {/* name digunakan untuk link navigasi nantinya */}
+        {/* komponen adalah page yang akan di tampilkan */}
+        {/* component yang pertama kali dipanggil akan jadi homepage */}
+        <Stack.Screen
+          name="TabScreen"
+          component={TabScreen}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="Details"
+          component={DetailRecipes}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="Popular"
+          component={PopulraScreen}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="List"
+          component={ListFood}
+          options={{headerShown: false}}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
